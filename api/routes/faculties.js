eb4c2743553b2c0b30af68c051ac45b70fa1eefc
SanjayNithin2002 <sanjaynithin.s2020@vitstudent.ac.in>
2023-02-16 02:05:23 +0530

    routes created

---
+++ api/routes/faculties.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+
+router.get('/', (req, res, next) => {
+   
+});
+
+router.post('/', (req, res, next) => {
+   
+});
+
+router.patch('/:id', (req, res, next) => {
+   
+});
+
+router.delete('/:id', (req, res, next) => {
+   
+});
+
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-const express = require('express');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const studentRoutes = require("./api/routes/students");
+const facultyRoutes = require("./api/routes/faculties");
 const app = express();
-app.get("/", (req,res)=> {
-    res.status(200).json({
-        message : "Working as indented"
+
+//Middleware
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+
+//Routes
+app.use("/students", studentRoutes);
+app.use("/faculties", facultyRoutes);
+
+//Error Handling
+
+app.use((req, res, next) => {
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        error : {
+            message : err.message
+        }
     });
 });
 
